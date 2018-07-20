b696cdec3d4303aeb422108ecf3b1eb6ae2b5d51
Michel73 <michael.kopf@sealsystems.de>
2018-07-20 17:44:55 +0200

    Added customized template for list component.

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,18 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterContentInit, Component, ContentChild, Input, OnInit, TemplateRef } from '@angular/core';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, AfterContentInit {
 
   @Input()
   entries: any[];
 
+  @ContentChild(TemplateRef) entriesTemplate: TemplateRef<any>;
+  hasCustomTemplate: boolean;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterContentInit() {
+    this.hasCustomTemplate = this.entriesTemplate != null;
   }
 
 }
